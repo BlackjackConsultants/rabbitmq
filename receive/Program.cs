5c7130b8539d4de794811d18b43631ade7a6abaa
jorge perez <jorgeperez00@hotmail.com>
2019-11-13 10:18:32 -0500

    both projects working

--- receive/Program.cs
+++ receive/Program.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace receive {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                UserName = "jorgeperez",
+                Password = "Sacalaapasear#1"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
